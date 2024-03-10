5e1271f070529614f4c824f78f50684f0d4e3ed5
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2024-03-11 02:29:35 +0300

    works after basic mobile refactor

--- tests/mobile/test_android.py
+++ tests/mobile/test_android.py
@@ -1,95 +1,87 @@
 import os
-import time
 
-from appium.webdriver.common.touch_action import TouchAction
+import time
 from appium.webdriver.common.appiumby import AppiumBy
-from selene import browser, be, command, have
+from selene import browser, be, have
 
 from notion_tests.utils.verification import get_code_from_email
 
 
 def test_login():
-
-
     google = True
     if google:
         browser.all((AppiumBy.XPATH, '//android.widget.Button')).element_by(have.text('Continue with Google')).click()
     else:
-        browser.element((AppiumBy.XPATH, '//android.widget.Button[@text="Continue with email"]')).click()
-        browser.element((AppiumBy.XPATH, '//android.widget.EditText[@resource-id="notion-email-input-2"]')).type(os.getenv('LOGIN'))
-        if browser.element((AppiumBy.XPATH, '//android.widget.Button[@text="Continue"]')).matching(be.present):
-            browser.element((AppiumBy.XPATH, '//android.widget.Button[@text="Continue"]')).click()
+        browser.all((AppiumBy.XPATH, '//android.widget.Button')).element_by(have.text("Continue with email")).click()
+        browser.element((AppiumBy.XPATH, '//android.widget.EditText[@resource-id="notion-email-input-2"]')).type(
+            os.getenv('LOGIN'))
+        if browser.all((AppiumBy.XPATH, '//android.widget.Button')).element_by(have.text('Continue')).matching(
+                be.present):
+            browser.all((AppiumBy.XPATH, '//android.widget.Button')).element_by(have.text('Continue')).click()
         else:
-            browser.element((AppiumBy.XPATH, '//android.widget.Button[@text="Continue with email"]')).click()
+            browser.all((AppiumBy.XPATH, '//android.widget.Button')).element_by(
+                have.text('Continue with email')).click()
 
-        # code = get_code_from_email()
-        code = 'nos-qupty-kosta-errim'
+        code = get_code_from_email()
+
         browser.driver.execute_script('mobile: hideKeyboard')
-        # browser.all((AppiumBy.CLASS_NAME, 'android.widget.EditText'))[-1].type(code)
         browser.all((AppiumBy.CLASS_NAME, 'android.widget.EditText'))[-1].type(code)
-        browser.element((AppiumBy.XPATH, '//android.widget.Button[@text="Continue with login code"]')).click()
+        browser.all((AppiumBy.XPATH, '//android.widget.Button')).element_by(
+            have.text('Continue with login codel')).click()
         # if self.incorrect_code.matching(be.present):
         #     code = get_code_from_email()
         #     self.password.clear().type(code).press_enter()
 
+    # WHEN
     browser.element((AppiumBy.ID, 'com.google.android.gms:id/account_display_name')).click()
-    browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Logging in to Notion…"]')).wait_until(be.absent)
+    browser.all((AppiumBy.XPATH, '//android.widget.TextView')).element_by(
+        have.text('Logging in to Notion…')).wait_until(be.absent)
     browser.element((AppiumBy.ID, 'com.android.permissioncontroller:id/permission_allow_button')).click()
 
-
+    # THEN
     browser.element((AppiumBy.XPATH, '//android.view.View[@resource-id="navigate_to_home"]')).should(be.present)
     browser.element((AppiumBy.XPATH, '//android.view.View[@resource-id="navigate_to_search"]')).should(be.present)
     browser.element((AppiumBy.XPATH, '//android.view.View[@resource-id="navigate_to_allUpdates"]')).should(be.present)
     browser.element((AppiumBy.XPATH, '//android.view.View[@resource-id="navigate_to_addPage"]')).should(be.present)
 
-    # browser.element((AppiumBy.XPATH, '//android.widget.Button[@text="⚾ Основы"]')).should(be.present)
-    # browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Основы"]')).should(be.present)
 
-    # test_create_page
+def test_create_page():
+    test_login()
+
+    # WHEN
     browser.element((AppiumBy.XPATH, '//android.view.View[@resource-id="navigate_to_addPage"]')).click()
-    # THEN
     browser.element((AppiumBy.CLASS_NAME, 'android.widget.EditText')).click()
-    # browser.element((AppiumBy.XPATH, '//android.widget.EditText')).send_keys('Appium')
     browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Choose a template..."]')).wait_until(be.visible)
-    time.sleep(4)
+    time.sleep(2)
     browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Choose a template..."]')).click()
     browser.element((AppiumBy.XPATH, '//android.widget.Button[@text="Reading List"]')).click()
     browser.element((AppiumBy.XPATH, '//android.widget.Button[@text="Use"]')).click()
-    browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Reading List"]')).should(be.present)
-
-
-
-    # test_search_page
-    browser.driver.execute_script('mobile: hideKeyboard')
-    browser.element((AppiumBy.XPATH, '//android.view.View[@resource-id="navigate_to_search"]')).click()
-    time.sleep(5)
-    browser.element((AppiumBy.XPATH, '//android.widget.EditText')).send_keys('Getting Started')
-    # class name android.widget.EditText
-    # //android.widget.EditText/android.view.View
 
     # THEN
-    # browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Getting Started"]')).should(be.present)
-    # browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Getting Started"]')).click()
-    browser.all((AppiumBy.XPATH, '//android.widget.TextView')).element_by(have.text('Getting Started')).should(be.present)
+    browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Reading List"]')).should(be.present)
+
+def test_search_page():
+    test_login()
+
+    # WHEN
+    browser.element((AppiumBy.XPATH, '//android.view.View[@resource-id="navigate_to_search"]')).click()
+    # time.sleep(5)
+    browser.element((AppiumBy.XPATH, '//android.widget.EditText')).send_keys('Getting Started')
+
+    # THEN
+    browser.all((AppiumBy.XPATH, '//android.widget.TextView')).element_by(have.text('Getting Started')).should(
+        be.present)
     browser.all((AppiumBy.XPATH, '//android.widget.TextView')).element_by(have.text('Getting Started')).click()
 
-    # test_delete_page
-    browser.driver.execute_script('mobile: hideKeyboard')
-    time.sleep(5)
+def test_delete_page():
+    test_login()
+    time.sleep(2)
     browser.element((AppiumBy.XPATH, '//android.view.View[@resource-id="navigate_to_home"]')).click()
-    time.sleep(5)
+    time.sleep(2)
     browser.all((AppiumBy.CLASS_NAME, 'android.widget.TextView')).element_by(have.text('Reading List')).click()
     browser.all((AppiumBy.CLASS_NAME, 'android.widget.Button'))[4].click()
-    # browser.all((AppiumBy.CLASS_NAME, 'android.view.MenuItem'))[-1].click()
     browser.all((AppiumBy.CLASS_NAME, 'android.view.MenuItem')).element_by(have.text('Delete')).click()
     browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Reading List"]')).should(be.absent)
-    # browser.element((AppiumBy.XPATH, '//android.view.View[@resource-id="navigate_to_search"]')).click()
-    # browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Pages, databases, text…"]')).type('Reading List')
-    # # THEN
-    # browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Reading List"]')).should(be.absent)
-
-    # -27 android.view.View
-
 
 # def test_onboarding():
 #     with allure.step('1й экран'):
